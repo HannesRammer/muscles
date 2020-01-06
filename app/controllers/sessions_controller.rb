f211f9a34a60972b6744fce816677783f204afb5
Hannes Rammer <Hannes.Rammer@gmail.com>
2020-01-06 03:28:10 +0100

    fixed signup and login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,17 +7,15 @@ class SessionsController < ApplicationController
     #user = User.authenticate(params[:login], params[:password])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      flash[:notice] = 'Logged in successfully.'
-      redirect_to root_path
+      redirect_to root_url, notice: 'Logged in successfully.'
     else
-      flash.now[:error] = 'Invalid login or password.'
-      render :action => 'new'
+      flash.now[:alert] = 'Invalid login or password.'
+      render 'new'
     end
   end
   
   def destroy
     session[:user_id] = nil
-    flash[:notice] = 'You have been logged out.'
-    redirect_to root_path
+    redirect_to root_url, notice: 'You have been logged out.'
   end
 end
