649e251dfc5e2f460400b6f497932f0a2befd5c9
HannesRammer <hannes.rammer@gmail.com>
2020-07-23 02:35:42 +0200

    add crawler and dataadd crawler and data

--- doc/crawler.js
+++ doc/crawler.js
@@ -1,10 +1,10 @@
 // ==UserScript==
-// @name         New Userscript
+// @name         https://www.skimble.com/​ crawler
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://www.spotebi.com/exercise-guide/*
+// @match        https://www.skimble.com/exercises/*
 // @require https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js
 // @grant        none
 // ==/UserScript==
@@ -13,7 +13,7 @@
     'use strict';
 
     // a function that loads jQuery and calls a callback function when jQuery has finished loading
-
+    let dbData = {};
     const dbconnect = window.indexedDB.open('exercises', 1);
     dbconnect.onupgradeneeded = ev => {
         console.log('Upgrade DB');
@@ -31,16 +31,43 @@
         const transaction = db.transaction('Exercise', 'readwrite');
         const store = transaction.objectStore('Exercise');
 
+        const query = store.openCursor();
+        query.onerror = ev => {
+            console.error('Anfrage fehlgeschlagen!', ev.target.error.message);
+        };
 
-        let name = "body > div.site-container > div.site-inner > div > main > article > div > h2:nth-child(5)";
-        let primary_selector = "body > div.site-container > div.site-inner > div > main > article > div > p:nth-child(4) > strong:nth-child(1)";
-        let secondary_selector = "body > div.site-container > div.site-inner > div > main > article > div > p:nth-child(4) > strong:nth-child(3)";
-        let tags = "body > div.site-container > div.site-inner > div > main > article > div > p:nth-child(4) > strong:nth-child(5)";
+        // Verarbeitung der Einzel-Query
+        query.onsuccess = ev => {
+            if (query.result) {
+                console.log('Datensatz 1', query.result.Name, query.result.url);
+            } else {
+                console.warn('Kein Eintrag vorhanden!');
+            }
+        };
 
-        let exercise_name = document.querySelector(name)?.innerHTML || "";
-        let primary_muscles = document.querySelector(primary_selector)?.innerHTML || "";
-        let secondary_muscles = document.querySelector(secondary_selector)?.innerHTML || "";
-        let tag_namess = document.querySelector(tags)?.innerHTML || "";
+        query.onsuccess = ev => {
+            const cursor = ev.target.result;
+            if (cursor) {
+                console.log(cursor.key, cursor.value.Name, cursor.value.url);
+                cursor.continue();
+            } else {
+                console.log('Keine Einträge mehr vorhanden!');
+            }
+        };
+
+        console.log('DB daten ausgegeben');
+        let name = "#main-content-container > div.carousel.main-carousel.content-header-main-carousel > div > div > div > div > div > div.span8.fill-height > h1";
+        let primary_selector = "#main-content-container > div.carousel.main-carousel.content-header-main-carousel > div > div > div > div > div > div.span8.fill-height > div.content-details-metadata > p:nth-child(1) > span.content-detail-list-items";
+        let secondary_selector = "#main-content-container > div.carousel.main-carousel.content-header-main-carousel > div > div > div > div > div > div.span8.fill-height > div.content-details-metadata > p:nth-child(2)";
+        let tags = "#main-content-container > div.carousel.main-carousel.content-header-main-carousel > div > div > div > div > div > div.span8.fill-height > div.content-details-metadata > p:nth-child(3) > span.content-detail-list-items";
+        let tags2 = "#main-content-container > div.carousel.main-carousel.content-header-main-carousel > div > div > div > div > div > div.span8.fill-height > div.content-details-metadata > p:nth-child(4) > span.content-detail-list-items";
+
+        let exercise_name = document.querySelector(name) ? document.querySelector(name).innerText.split(": ")[1] : "";
+        let primary_muscles = document.querySelector(primary_selector) ? document.querySelector(primary_selector).innerText.split(", ").toString() : "";
+        let secondary_muscles = document.querySelector(secondary_selector) ? document.querySelector(secondary_selector).innerText.split(", ").toString() : "";
+        let tag_namess = document.querySelector(tags) ? document.querySelector(tags).innerText.split(", ").toString() : "";
+        let tag_namess2 = document.querySelector(tags2) ? document.querySelector(tags2).innerText.split(", ").toString() : "";
+        let all_tags = tag_namess+","+tag_namess2;
         let url = document.location.href;
 
         /*
@@ -56,7 +83,7 @@
             Name: exercise_name,
             primary: primary_muscles,
             secondary: secondary_muscles,
-            tags: tag_namess,
+            tags: all_tags,
             url: url
         });
         // let exercise_links = document.querySelectorAll(".grids-frame li p a");
@@ -69,7 +96,7 @@
             console.log('Daten wurden erfolgreich hinzugefügt!');
             const store = db.transaction('Exercise', 'readonly').objectStore('Exercise');
             //const query = store.get(1); // Einzel-Query
-            const query = store.openCursor()
+            const query = store.openCursor();
             query.onerror = ev => {
                 console.error('Anfrage fehlgeschlagen!', ev.target.error.message);
             };
@@ -86,25 +113,37 @@
             query.onsuccess = ev => {
                 const cursor = ev.target.result;
                 if (cursor) {
-                    console.log(cursor.key, cursor.value.Name, cursor.value.url);
+                    dbData[cursor.key] = {
+                        name:cursor.value.Name,
+                        primary:cursor.value.primary,
+                        secondary:cursor.value.secondary,
+                        tags: cursor.value.tags,
+                        url: cursor.value.url
+                    };
                     cursor.continue();
                 } else {
-                    console.log('Keine Einträge mehr vorhanden!');
+                    debugger;
+                    console.log(dbData.toString());
                 }
             };
         };
     }
 
-        function crawling(exercise_links, i) {
+
+
+    // Your code here...
+})();
+/*
+function crawling(exercise_links, i) {
             if (exercise_links === null) {
-                exercise_links = document.querySelectorAll(".grids-frame li p a");
+               exercise_links = document.querySelectorAll(".row-fluid ul.thumbnails li a");
             }
-            if (i+1 < exercise_links.length) {
-                window.open(exercise_links[i].href, "exercise");
+            if (i+1 <= exercise_links.length) {
+                window.open(exercise_links[i].href, "exercises");
 
                 setTimeout(function () {
                     crawling(exercise_links, i+1)
-                }, 2000);
+                }, 1250);
 
             }
 
@@ -112,10 +151,5 @@
         }
 
         crawling(null,0);
-    // Your code here...
-})();
-
-
-
-
+*/
 
