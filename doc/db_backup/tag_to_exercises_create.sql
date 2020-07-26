bc18bd0651ce101e9fb8ef2921da3345a53ab925
Hannes Rammer <Hannes.Rammer@gmail.com>
2020-07-26 08:52:40 +0200

    -update  - -added  -update db data
    -fixed
     -
    
    -removed
     -

--- doc/db_backup/tag_to_exercises_create.sql
+++ doc/db_backup/tag_to_exercises_create.sql
@@ -1,8 +1,8 @@
-CREATE TABLE `tag_to_exercise` (
+CREATE TABLE `tag_to_exercises` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `tag_id` int(11) DEFAULT NULL,
   `exercise_id` int(11) DEFAULT NULL,
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=12157 DEFAULT CHARSET=utf8;
