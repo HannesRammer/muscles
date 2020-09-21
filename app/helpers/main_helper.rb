3935ee507d6578c7e1226ab39b41b134042b0ef8
Hannes Rammer <Hannes.Rammer@gmail.com>
2020-09-21 18:12:57 +0200

    -update  -fix german to english
    -added
     -
    
    -fixed
     -
    
    -removed
     -

--- app/helpers/main_helper.rb
+++ app/helpers/main_helper.rb
@@ -2,12 +2,12 @@ module MainHelper
   def add_to_list_link(exercise)
     link_to("<i class='material-icons vertical_icon' >playlist_add</i>".html_safe,
             {:controller => "trainingsplans", :action => "load_trainingsplans", :exercise => exercise.id},
-            {:remote => true, :method => :post, :title => "add to trainingsplan", :class => "add_to_playlist"}) if logged_in?
+            {:remote => true, :method => :post, :title => "add to workout", :class => "add_to_playlist"}) if logged_in?
   end
 
   def add_to_list_with_trainingsplan(exercise, trainingsplan)
-    link_to("<i class='material-icons vertical_icon' title='Zum Trainingsplan #{trainingsplan.name} hinzufügen'>add_circle_outline</i>".html_safe,
+    link_to("<i class='material-icons vertical_icon' title='add to workout #{trainingsplan.name}'>add_circle_outline</i>".html_safe,
             {:controller => "main", :action => "add_exercise", :exercise => exercise.id, :trainingsplan_id => trainingsplan.id},
-            {:remote => true, :method => :post, :title => "add to trainingsplan", :class => "add_to_playlist"})
+            {:remote => true, :method => :post, :title => "add to workout", :class => "add_to_playlist"})
   end
 end
