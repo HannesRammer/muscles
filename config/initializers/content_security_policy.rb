04b2a2982f6df76b2961545892d4c445c2445271
Hannes Rammer <Hannes.Rammer@gmail.com>
2021-04-18 16:48:07 +0200

    -update  - -added  -content security policy
    -fixed
     -
    
    -removed
     -

---
+++ config/initializers/content_security_policy.rb
@@ -0,0 +1,15 @@
+Rails.application.config.content_security_policy do  |p|
+    p.default_src :self, :https
+    p.font_src    :self, :https
+    # p.img_src     :self , :https
+    p.object_src  :none
+    p.script_src  :self, :https
+    p.style_src   :self, :https, :unsafe_inline
+    p.img_src :self, :https
+    # p.img_src :self, 'power.itp.ac.cn', :https
+    # p.img_src 'power.itp.ac.cn'
+    # p.img_src :none
+    # p.img_src :self
+    # Specify URI for violation reports
+    p.report_uri  "/violation_report"
+end
